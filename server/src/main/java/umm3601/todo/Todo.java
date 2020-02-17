2ec21b31547c9f03eaa0b859e3dae09c22ceabcc
Shawn Reuter <reute078@morris.umn.edu>
2020-02-16 23:29:35 -0600

    Recommit Comparators

--- server/src/main/java/umm3601/todo/Todo.java
+++ server/src/main/java/umm3601/todo/Todo.java
@@ -8,4 +8,49 @@ public class Todo{
   public String body;
   public String category;
 
+  public String getId() {
+    return _id;
+  }
+  public String getOwner(){
+    return owner;
+  }
+  public boolean getStatus(){
+    return status;
+  }
+  public String getBody(){
+    return body;
+  }
+  public String getCategory(){
+    return category;
+  }
+
+public class TodoOwnerComparator implements Comparable<Todo>{
+
+   @Override
+   public int compareTo(Todo other) {
+     return _id.compareTo(other._id);
+   }
+}
+public class TodoBodyComparator implements Comparable<Todo>{
+
+  @Override
+  public int compareTo(Todo other) {
+    return body.compareTo(other.body);
+  }
+}
+public class TodoStatusComparator implements Comparable<Todo>{
+
+  @Override
+  public int compareTo(Todo other) {
+    return status.compareTo(other.status);
+  }
+}
+public class TodoCategoryComparator implements Comparable<Todo>{
+
+  @Override
+  public int compareTo(Todo other) {
+    return category.compareTo(other.category);
+  }
+}
+
 }
